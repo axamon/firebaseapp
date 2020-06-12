e6766ce69bc4355514799d416205df8c4303d588
Alberto Bregliano <alberto.bregliano@pm.me>
2020-06-12 20:03:10 +0200

    vendorized

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/axamon/firebaseapp
 
 go 1.13
+
+require (
+	cloud.google.com/go/firestore v1.2.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	google.golang.org/api v0.26.0
+)
--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
-import "flag"
+import (
+	"context"
+	"flag"
+	"log"
+
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
+)
 
 var auth = flag.String("auth", "auth.json", "File autenticazione  di google")
 
 func main() {
 
 	flag.Parse()
+
+	// Use a service account
+	ctx := context.Background()
+	sa := option.WithCredentialsFile(*auth)
+	app, err := firebase.NewApp(ctx, nil, sa)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer client.Close()
+
+	_, _, err = client.Collection("users").Add(ctx, map[string]interface{}{
+		"first": "Ada",
+		"last":  "Lovelace",
+		"born":  1815,
+	})
+	if err != nil {
+		log.Fatalf("Failed adding alovelace: %v", err)
+	}
 
 }
